e9f7601f4208637100713be702a718887beb0684
Antoinettewangari <antoinettewangari@gmail.com>
2019-05-08 09:44:04 +0300

    message

---
+++ anto/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AntoConfig(AppConfig):
+    name = 'anto'
---
+++ anto/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from .models import Member
+from django.views.generic import ListView,CreateView,UpdateView, DeleteView
+
+
+# Create your views here.
+class MemberListView(ListView):
+    model =Member
+    template_name = 'anto/index.html'
+    context_object_name = 'profiles'
+    ordering=['name']
+    
+    def get_queryset(self):
+        query=self.request.GET.get('q')
+        if query:
+            return Member.objects.filter(name__icontains=query)
+        else:
+            return Member.objects.all()
+
+class MemberCreateView(CreateView):
+    model = Member
+    fields = '__all__'
+
+class MemberUpdateView(UpdateView):
+    model = Member
+    fields = '__all__'
+
+class MemberDeleteView(DeleteView):
+    model = Member
+    success_url = '/'
